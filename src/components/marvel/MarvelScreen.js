d5177f3a87b0e29c2f0ee51d67b5e5f2a214274f
facureyes <facundotomasreyes@gmail.com>
2021-06-29 00:08:27 -0300

    Show heroes list, filtered by publisher

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import HeroesList from '../heroes/HeroesList'
 
 const MarvelScreen = () => {
   return (
-    <h1>
-      Marvel Screen
-    </h1>
+    <>
+      <h1>Marvel Screen</h1>
+      <hr />
+      <HeroesList publisher="Marvel Comics" />
+    </>
   )
 }
 
