bdbe15280a2d2a9b9ac032c16e0ee8726d44abdc
facureyes <facundotomasreyes@gmail.com>
2021-06-29 00:09:58 -0300

    Added redirect after login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
-const LoginScreen = () => {
+
+const LoginScreen = ({ history }) => {
+  
+  const handleClick = () => {
+    // Push add a new path to history and let user go back
+    // history.push('/');
+    // Replace as it name indicates, replaces the actual path in history, so user wont be able to navigate back
+    history.replace('/') ;
+  }
   return (
-    <h1>
-      Login Screen
-    </h1>
+    <div className="container mt-5">
+      <h1>Login Screen</h1>
+      <hr />
+      <button
+        className="btn btn-dark"
+        onClick={handleClick}
+      >
+        Login
+      </button>
+    </div>
   )
 }
 
