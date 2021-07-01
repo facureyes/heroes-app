65e8a5bcb5688430be30f47bd0d85daf6bdcb119
facureyes <facundotomasreyes@gmail.com>
2021-06-30 23:28:49 -0300

    Created hero screen

--- src/components/heroes/HeroeScreen.js
+++ src/components/heroes/HeroeScreen.js
@@ -1,10 +1,57 @@
 import React from 'react'
+import { useParams, Redirect } from 'react-router-dom'
+import { getHeroesById } from '../../selectors/getHeroeById';
 
-const HeroeScreen = () => {
+const HeroeScreen = ({history}) => {
+  
+  const { heroeId } = useParams();
+  const {id, 
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters } = getHeroesById(heroeId) || {};
+  if(!id){
+    return <Redirect to="/" />;
+  }
+
+  const handleReturn = () => {
+    if(history.length <= 2){
+      history.push('/')
+    } else {
+      history.goBack()
+    }
+  }
+
+
   return (
-    <h1>
-      Heroe Screen
-    </h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={`../assets/heroes/${heroeId}.jpg`}
+          alt={ superhero }
+          className="img-thumbnail"
+        />
+      </div>
+      <div className="col-4">
+        <h3>{ superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b> {alter_ego}</li>
+          <li className="list-group-item"><b>Publisher: </b> {publisher}</li>
+          <li className="list-group-item"><b>First Appearance: </b> {first_appearance}</li>
+        </ul>
+
+        <h5>Characters</h5>
+        <p>{ characters }</p>
+
+        <button 
+          className="btn btn-outline-dark"
+          onClick={handleReturn}
+        >
+          Return
+        </button>
+      </div>
+    </div>
   )
 }
 
