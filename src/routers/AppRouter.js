67aa5df64ccf2e4856321d1bd7c122ca3a0d3555
Facundo Reyes <facundotomasreyes@gmail.com>
2021-07-15 13:09:26 -0300

    Managed public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,19 +3,19 @@ import Proptypes from 'prop-types';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
 } from "react-router-dom";
 import { AuthContext } from '../auth/AuthContext';
 import LoginScreen from '../components/login/LoginScreen';
 import DashboardRoutes from './DashboardRoutes';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 const AppRouter = () => {
   const {user: {logged}} = useContext(AuthContext);
   return (
     <Router>
       <Switch>
-        <Route exact path="/login" component={ LoginScreen } />
+        <PublicRoute isAuthenticated={logged} exact path="/login" component={ LoginScreen } />
         <PrivateRoute isAuthenticated={logged} path="/" component={ DashboardRoutes } />
       </Switch>
     </Router>
