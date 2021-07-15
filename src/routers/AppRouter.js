7a3109da075f25141efb7594da7196fbbe550329
Facundo Reyes <facundotomasreyes@gmail.com>
2021-07-15 13:02:13 -0300

    Managed private routes.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Proptypes from 'prop-types';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
+import { AuthContext } from '../auth/AuthContext';
 import LoginScreen from '../components/login/LoginScreen';
 import DashboardRoutes from './DashboardRoutes';
+import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => {
+  const {user: {logged}} = useContext(AuthContext);
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={ LoginScreen } />
-        <Route path="/" component={ DashboardRoutes } />
+        <PrivateRoute isAuthenticated={logged} path="/" component={ DashboardRoutes } />
       </Switch>
     </Router>
   )
+}
+
+PrivateRoute.propTypes = {
+  isAuthenticated: Proptypes.bool.isRequired,
+  component: Proptypes.func.isRequired,
 }
 
 export default AppRouter
---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+
+const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => {
+  return (
+    <Route {...rest}
+      component={ (props) => {
+          return ( isAuthenticated ? 
+            <Component {...props} /> :
+            <Redirect to='/login' />
+          )
+        }
+      }
+    />
+  )
+}
+
+export default PrivateRoute
